deebc5ecacb1be03a49b7b146ab568f655f7fd1b
Jakobkiryo <143487429+Jakobkiryo@users.noreply.github.com>
2024-05-06 11:07:53 +0200

    Added Unit test

---
+++ TurboCollections.Test/TurboMathsTest.cs
@@ -0,0 +1,13 @@
+
+
+namespace TurboCollections.Test;
+
+public static class TurboMathsTests
+{
+    [Test]
+    public static void SayHelloExists()
+    {
+        TurboMaths.SayHello();
+        Assert.Pass();
+    }
+}
