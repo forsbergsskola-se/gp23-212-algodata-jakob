675b7ce0008816f5963dd7a26c5414516f0b1a18
Jakobkiryo <143487429+Jakobkiryo@users.noreply.github.com>
2024-05-06 11:53:30 +0200

    Worked on the iterator exerrcise not finished

---
+++ P1_3_Iterator/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+//Create a List (C#) or vector (C++) with 5 numbers: 1, 1, 2, 3, 5.
+//C#: Assign it to a Variable of Type IEnumerable.
+//Use GetEnumerator() (C#) a while-Loop to print all elements of the List-Variable 
+List<int> numberList = new() { 1, 1, 2, 3, 5 };
+
+IEnumerable<int> numbers = numberList;
+IEnumerator<int> iterator = numbers.GetEnumerator();
+
+while (iterator.MoveNext())
+{
+    Console.WriteLine(iterator.Current);
+}
+
+//
+
+
+
