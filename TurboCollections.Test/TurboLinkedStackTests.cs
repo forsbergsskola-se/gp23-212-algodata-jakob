db34792f993fd568a3303744bcfb7a64882d0873
Jakobkiryo <143487429+Jakobkiryo@users.noreply.github.com>
2024-05-16 11:01:12 +0200

    Started writing unit test for linkedstacktest exercise

--- TurboCollections.Test/TurboLinkedStackTests.cs
+++ TurboCollections.Test/TurboLinkedStackTests.cs
@@ -2,5 +2,37 @@
 
 public class TurboLinkedStackTests
 {
+    [Test]
+    public void TestPush()
+    {
+        var stack = new TurboLinkedStack<int>();
+
+    }
+    
+    [Test]
+    public void TestPeek()
+    {
+        
+    }
+
+    
+    [Test]
+    public void TestPop()
+    {
+        
+    }
+    
+    [Test]
+    public void TestClear()
+    {
+        
+    }
+    
+    
+    [Test]
+    public void TestCount()
+    {
+        
+    }
     
 }
