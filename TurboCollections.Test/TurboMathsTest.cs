959c4edc387c08c854ad706bbb59b3e7af85c74d
Jakobkiryo <143487429+Jakobkiryo@users.noreply.github.com>
2024-05-16 12:06:57 +0200

    forgot to write unit test, one keeps failing and i dont know why

--- TurboCollections.Test/TurboMathsTest.cs
+++ TurboCollections.Test/TurboMathsTest.cs
@@ -11,4 +11,23 @@ public static class TurboMathsTests
         Assert.Pass();
     }
     
+    //P1_3
+    
+    [Test]
+    public static void TestGetEvenNumbers()
+    {
+        CollectionAssert.AreEqual(new[] { 0, 2, 4, 6, 8, 10, 12 }, TurboMaths.GetEvenNumbers(12));
+        CollectionAssert.AreEqual(new[] { 0, 2, 4, 6, 8, 10, 12, 14 }, TurboMaths.GetEvenNumbers(15));
+        CollectionAssert.AreNotEqual(new[] {0}, TurboMaths.GetEvenNumbers(-1));
+        CollectionAssert.AreEqual(new[] { 0 }, TurboMaths.GetEvenNumbers(0));
+    }
+    
+    [Test]
+    public static void TestGetEvenNumbersList()
+    {
+        CollectionAssert.AreEqual(new [] {0,2,4,6,8,10,12} , TurboMaths.GetEvenNumbersList(12));
+        CollectionAssert.AreEqual(new[] { 0,2, 4, 6, 8, 10, 12, 14 }, TurboMaths.GetEvenNumbersList(15));
+        CollectionAssert.AreNotEqual(new[] {0}, TurboMaths.GetEvenNumbersList(-1));
+        CollectionAssert.AreEqual(new[] { 0 }, TurboMaths.GetEvenNumbersList(0));
+    }
 }
