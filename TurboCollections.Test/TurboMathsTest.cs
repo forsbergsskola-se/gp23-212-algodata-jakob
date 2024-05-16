cf8fffddeaab7aa6930d1d5250bd1fc59de2b40f
Jakobkiryo <143487429+Jakobkiryo@users.noreply.github.com>
2024-05-16 12:24:40 +0200

    Now it got fixed dont know how, just readded the code manually

--- TurboCollections.Test/TurboMathsTest.cs
+++ TurboCollections.Test/TurboMathsTest.cs
@@ -10,24 +10,25 @@ public static class TurboMathsTests
         TurboMaths.SayHello();
         Assert.Pass();
     }
-    
+
     //P1_3
-    
+
     [Test]
     public static void TestGetEvenNumbers()
     {
         CollectionAssert.AreEqual(new[] { 0, 2, 4, 6, 8, 10, 12 }, TurboMaths.GetEvenNumbers(12));
         CollectionAssert.AreEqual(new[] { 0, 2, 4, 6, 8, 10, 12, 14 }, TurboMaths.GetEvenNumbers(15));
-        CollectionAssert.AreNotEqual(new[] {0}, TurboMaths.GetEvenNumbers(-1));
+        CollectionAssert.AreNotEqual(new[] { 0 }, TurboMaths.GetEvenNumbers(-1));
         CollectionAssert.AreEqual(new[] { 0 }, TurboMaths.GetEvenNumbers(0));
     }
-    
+
+
     [Test]
     public static void TestGetEvenNumbersList()
     {
-        CollectionAssert.AreEqual(new [] {0,2,4,6,8,10,12} , TurboMaths.GetEvenNumbersList(12));
-        CollectionAssert.AreEqual(new[] { 0,2, 4, 6, 8, 10, 12, 14 }, TurboMaths.GetEvenNumbersList(15));
-        CollectionAssert.AreNotEqual(new[] {0}, TurboMaths.GetEvenNumbersList(-1));
-        CollectionAssert.AreEqual(new[] { 0 }, TurboMaths.GetEvenNumbersList(0));
+        CollectionAssert.AreEqual(new[] { 0, 2, 4, 6, 8, 10, 12 }, TurboMaths.GetEvenNumbers(12));
+        CollectionAssert.AreEqual(new[] { 0, 2, 4, 6, 8, 10, 12, 14 }, TurboMaths.GetEvenNumbers(15));
+        CollectionAssert.AreNotEqual(new[] { 0 }, TurboMaths.GetEvenNumbers(-1));
+        CollectionAssert.AreEqual(new[] { 0 }, TurboMaths.GetEvenNumbers(0));
     }
 }
