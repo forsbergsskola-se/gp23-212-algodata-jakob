e435affd1375ca0d0445aa67a4c1d6f07b3efa1b
Jakobkiryo <143487429+Jakobkiryo@users.noreply.github.com>
2024-05-22 10:40:20 +0200

    Started setting up the google test for the linked queue exercise

--- TurboCollections.Test/TurboLinkedQueueTest.cs
+++ TurboCollections.Test/TurboLinkedQueueTest.cs
@@ -2,5 +2,36 @@
 
 public class TurboLinkedQueueTest
 {
+    [Test]
+    public void TestCount()
+    {
+        
+    }
+    
+    [Test]
+    public void TestEnqueue()
+    {
+        
+    }
+
+    
+    [Test]
+    public void TestPeek()
+    {
+        
+    }
+    
+    [Test]
+    public void TestDequeue()
+    {
+        
+    }
+    
+    
+    [Test]
+    public void TestClear()
+    {
+        
+    }
     
 }
